bb5a0bfd6f4d1780c3f3fa87b04d6d5e93a35963
Giulio-Luiz-Valcanaia <giuliovalcanaia@gmail.com>
2026-09-17 15:08:05 -0300

    feat: nova página de login
    Foi inserida uma nova página de login com base nos blocos prontos do
    shadcn https://ui.shadcn.com/blocks

--- atividades-em-lista/app/login/page.tsx
+++ atividades-em-lista/app/login/page.tsx
@@ -1,67 +1,33 @@
-import { Button, buttonVariants } from "@/components/ui/button"
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import Link from "next/link"
+"use client"
 
-export default function Login() {
+import { LoginForm } from "@/components/login-form"
+import { GalleryVerticalEndIcon } from "lucide-react"
+
+export default function LoginPage() {
   return (
-    <main className="flex min-h-screen items-center justify-center p-4">
-      <Card className="w-full max-w-sm">
-        <CardHeader>
-          <CardTitle>Faça login na sua conta</CardTitle>
-          <CardAction>
-            <Button variant="link">Inscreva-se</Button>
-          </CardAction>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="nome@email.com"
-                  required
-                  autoFocus
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Senha</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Esqueceu sua senha?
-                  </a>
-                </div>
-                <Input id="password" type="password" required />
-              </div>
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <GalleryVerticalEndIcon className="size-4" />
             </div>
-          </form>
-        </CardContent>
-        <CardFooter className="flex-col gap-2">
-          <Button type="submit" className="w-full">
-            Entrar
-          </Button>
-          <Button variant="outline" className="w-full">
-            Entrar com o Google
-          </Button>
-          <a href={"/"} className={"underline-offset-4 hover:underline"}>
-            Voltar
+            GV Sistemas
           </a>
-        </CardFooter>
-      </Card>
-    </main>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="relative hidden bg-muted lg:block">
+        <img
+          src="https://ui.shadcn.com/placeholder.svg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
   )
 }
